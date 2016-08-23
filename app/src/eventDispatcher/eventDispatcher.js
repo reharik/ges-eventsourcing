d75a926140ca27ef6f46a514f1d8efe1affc6a15
Raif Harik <reharik@gmail.com>
2016-08-23 09:07:37 -0500

    missed one. at least

--- app/src/eventDispatcher/eventDispatcher.js
+++ app/src/eventDispatcher/eventDispatcher.js
@@ -16,10 +16,7 @@ var eventDispatcher = function eventDispatcher(eventstore,
 
                 var mAndF  = mapAndFilterStream(streamType);
                 var _s     = serveToHandlers(_handlers);
-                var stream = rx.Observable.fromCallback(eventstore.subscribeToAllFrom("$all", 0, {
-                    'username': "admin",
-                    'password': "changeit"
-                }, x=> {return x;})
+                var stream = rx.Observable.fromEvent(eventstore.subscribeToAllFrom(), 'event')
                     .filter(mAndF.isValidStreamType)
                     .map(mAndF.transformEvent   );
 
@@ -30,3 +27,35 @@ var eventDispatcher = function eventDispatcher(eventstore,
 };
 module.exports = eventDispatcher;
 
+// /**
+//  * Created by rharik on 6/18/15.
+//  */
+// "use strict";
+//
+// var eventDispatcher = function eventDispatcher(eventstore,
+//                                                logger,
+//                                                rx,
+//                                                R,
+//                                                mapAndFilterStream,
+//                                                serveToHandlers,treis) {
+//     return function(_handlers) {
+//         return {
+//             startDispatching: function(streamType) {
+//                 logger.info('startDispatching | startDispatching called');
+//
+//                 var mAndF  = mapAndFilterStream(streamType);
+//                 var _s     = serveToHandlers(_handlers);
+//                 var stream = rx.Observable.fromCallback(eventstore.subscribeToAllFrom("$all", 0, {
+//                     'username': "admin",
+//                     'password': "changeit"
+//                 }, x=> {return x;})
+//                     .filter(mAndF.isValidStreamType)
+//                     .map(mAndF.transformEvent   );
+//
+//                 stream.subscribe(_s.serveEventToHandlers);
+//             }
+//         }
+//     };
+// };
+// module.exports = eventDispatcher;
+//
