0e5e6f2d50efaa6ea71d1044f66d6e8d7e6593d3
Raif Harik <reharik@gmail.com>
2017-03-29 10:20:30 -0500

    debugging

--- app/src/readstoreRepository/eventHelperRepository.js
+++ app/src/readstoreRepository/eventHelperRepository.js
@@ -7,6 +7,10 @@ module.exports = function(R, _fantasy, appfuncs, uuid, logger, pgFuture) {
 
             var handleResult = x => {
                 const row = x.rows[0];
+                console.log(`==========row=========`);
+              console.log(row);
+              console.log(row.commitPossition);
+                console.log(`==========END row=========`);
                 const rowPosition = row && row.commitPosition ? row.commitPosition : -1;
                 logger.debug(`event commit possition ${originalEventNumber}. 
                     db commit possition ${rowPosition}.`);
