653de7fd7a62de77d6f04e2a3754fddcf3922428
Raif Harik <reharik@gmail.com>
2017-03-30 15:37:01 -0500

    fixing eventrepository

--- app/src/applicationFunctions/eventFunctions.js
+++ app/src/applicationFunctions/eventFunctions.js
@@ -3,9 +3,18 @@ module.exports = function(R, uuid, functionalHelpers){
   var fh = functionalHelpers;
   var parseMetadata = R.compose(R.chain(fh.safeParseBuffer), R.chain(fh.safeProp('metadata')), fh.safeProp('event'));
   var parseData = R.compose(R.chain(fh.safeParseBuffer), R.chain(fh.safeProp('data')), fh.safeProp('event'));
+  var incomingEvent = event => {
+    return {
+      eventName : (R.compose(R.chain(fh.safeProp('eventType')), fh.safeProp('event'))(event)).getOrElse(),
+      metadata: parseMetadata(event).getOrElse({}),
+      data: parseData(event).getOrElse({}),
+      originalPosition: fh.safeProp('originalPosition', event).getOrElse({})
+    };
+  };
 
   return {
     parseMetadata,
-    parseData
+    parseData,
+    incomingEvent
   }
 };
