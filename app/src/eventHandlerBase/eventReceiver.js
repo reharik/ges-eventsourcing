ffa5f98fb8120aa7504ab25bbd5172cc1a476c12
Raif Harik <reharik@gmail.com>
2017-05-27 15:37:53 -0500

    binding handler function to handler incase it's a stateful class

--- app/src/eventHandlerBase/eventReceiver.js
+++ app/src/eventHandlerBase/eventReceiver.js
@@ -2,7 +2,7 @@ module.exports = function (eventWorkflow, logger) {
 
     return function (source, handler) {
         source.subscribe(async function (x) {
-            const func = handler[x.eventName];
+            const func = handler[x.eventName].bind(handler);
             if (func) {
                 await eventWorkflow(x, handler.handlerName, func);
             }
