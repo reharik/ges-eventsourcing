301974f4002379dd242240492084a5f88c3a1942
Raif Harik <reharik@gmail.com>
2017-05-28 17:11:23 -0500

    better logging

--- app/src/eventHandlerBase/eventWorkflow.js
+++ app/src/eventHandlerBase/eventWorkflow.js
@@ -8,24 +8,26 @@ module.exports = function(dispatchNotification,
     var fh      = appfuncs.functionalHelpers;
     try {
       logger.trace(handlerName + ' ' + JSON.stringify(event));
-      const isIdempotent = await eventHelperRepository.checkIdempotency(fh.getSafeValue('commitPosition', event), handlerName);
-      logger.trace('message for ' + handlerName + ' isIdempotent ' + JSON.stringify(isIdempotent));
+      const isIdempotent = await eventHelperRepository
+        .checkIdempotency(fh.getSafeValue('commitPosition', event), handlerName);
+      logger.trace(`message ${event.eventName} for ${handlerName} isIdempotent ${JSON.stringify(isIdempotent)}`);
       if (!isIdempotent.isIdempotent) {
         throw new Error("item has already been processed");
       }
 
       var continuationId = R.view(R.lensProp('continuationId'), fh.getSafeValue('metadata', event));
       var handlerResult = await hnadlerFunction(fh.getSafeValue('data', event), continuationId);
-      logger.trace('message for ' + handlerName + ' was handled ' + event.eventName);
+      logger.trace(`message for ${handlerName} was handled ${event.eventName}`);
 
       await eventHelperRepository.recordEventProcessed(fh.getSafeValue('commitPosition', event), handlerName);
       logger.trace('message for ' + handlerName + ' recorded as processed ' + event.eventName);
 
       await dispatchNotification("Success", event, handlerResult);
-      logger.trace('message for ' + handlerName + ' notification disaptched');
+      logger.trace(`message ${eventName} for ${handlerName} notification disaptched`);
 
     } catch (ex) {
-      logger.debug(`An exception in event workflow thrown, this may just be non idempotent catch though`);
+      if(ex.message === "item has already been processed") { return; }
+      logger.debug(`An exception processing event ${eventName} in workflow: ${handlerName} was thrown`);
       logger.debug(ex);
 
       // here we need to determin if this is a catastrophic failure, and if so best to throw
