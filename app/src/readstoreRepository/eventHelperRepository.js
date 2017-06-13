cc671ac784c52f68ece1d5fdd2364dc947c1a2ef
Raif Harik <reharik@gmail.com>
2017-06-13 09:45:21 -0500

    found more problems

--- app/src/readstoreRepository/eventHelperRepository.js
+++ app/src/readstoreRepository/eventHelperRepository.js
@@ -1,26 +1,26 @@
-module.exports = function(pgasync, config, uuid, logger) {
-  const pg = new pgasync.default(config.postgres.config);
-
-  return {
-    async checkIdempotency(commitPosition, eventHandlerName) {
-      let query = 'SELECT * from "lastProcessedPosition" where "handlerType" = \'' + eventHandlerName + '\'';
-      logger.debug(query);
-      return await pg.query(query)
-        .then(response => {
-          const row = response.rows[0];
-          const rowPosition = row && row.commitPosition ? row.commitPosition : -1;
-          logger.debug(`event commit possition ${commitPosition}. 
+module.exports = function(pgasync, uuid, logger) {
+  return function(config) {
+    const pg = new pgasync.default(config);
+    return {
+      async checkIdempotency(commitPosition, eventHandlerName) {
+        let query = 'SELECT * from "lastProcessedPosition" where "handlerType" = \'' + eventHandlerName + '\'';
+        logger.debug(query);
+        return await pg.query(query)
+          .then(response => {
+            const row = response.rows[0];
+            const rowPosition = row && row.commitPosition ? row.commitPosition : -1;
+            logger.debug(`event commit possition ${commitPosition}. 
                     db commit possition ${rowPosition}.`);
-          let idempotent = parseInt(commitPosition) > parseInt(rowPosition);
-          let result = {isIdempotent: idempotent};
-          logger.debug(result);
-          return result;
-        });
-    },
+            let idempotent = parseInt(commitPosition) > parseInt(rowPosition);
+            let result = {isIdempotent: idempotent};
+            logger.debug(result);
+            return result;
+          });
+      },
 
-    async recordEventProcessed(commitPosition, eventHandlerName) {
+      async recordEventProcessed(commitPosition, eventHandlerName) {
 
-      let query = `WITH UPSERT AS (
+        let query = `WITH UPSERT AS (
  UPDATE "lastProcessedPosition"
  SET "commitPosition" = '${commitPosition}',
   "handlerType" =  '${eventHandlerName}'
@@ -30,8 +30,9 @@ module.exports = function(pgasync, config, uuid, logger) {
  SELECT '${uuid.v4() }' , '${commitPosition}', '${eventHandlerName }'
 WHERE NOT EXISTS ( SELECT 1 from "lastProcessedPosition" where "handlerType" = '${eventHandlerName}')`;
 
-      return await pg.query(query);
+        return await pg.query(query);
 
-    }
+      }
+    };
   };
 };
--- app/src/readstoreRepository/rsRepository.js
+++ app/src/readstoreRepository/rsRepository.js
@@ -1,116 +1,118 @@
-module.exports = function(pgasync, config, uuid, logger) {
-  const pg = new pgasync.default(config.postgres.config);
+module.exports = function(pgasync, uuid, logger) {
+  return function(config) {
 
-  return {
-    async getById(id, table) {
-      let query = (`SELECT * from "${table}" where "id" = '${id}'`);
-      logger.debug(query);
+    const pg = new pgasync.default(config);
+    return {
+      async getById(id, table) {
+        let query = (`SELECT * from "${table}" where "id" = '${id}'`);
+        logger.debug(query);
 
-      return await pg.query(query)
-        .then(result => {
-          const row = result.rows[0];
-          return row && row.document ? row.document : {};
-        });
-    },
+        return await pg.query(query)
+          .then(result => {
+            const row = result.rows[0];
+            return row && row.document ? row.document : {};
+          });
+      },
 
-    async getByIds(ids, table) {
-      let query = (`SELECT * from "${table}" where "id" in '(${ids.split(',')})'`);
-      logger.debug(query);
-      return await pg.query(query)
-        .then(result => {
-          const row = result.rows[0];
-          return row && row.document ? row.document : {};
-        });
-    },
+      async getByIds(ids, table) {
+        let query = (`SELECT * from "${table}" where "id" in '(${ids.split(',')})'`);
+        logger.debug(query);
+        return await pg.query(query)
+          .then(result => {
+            const row = result.rows[0];
+            return row && row.document ? row.document : {};
+          });
+      },
 
-    async save(table, document, id) {
-      try {
-        let query;
-        if (id) {
-          query = `UPDATE "${table}" SET document = '${JSON.stringify(document)}' where id = '${id}'`;
-        } else {
-          query = `INSERT INTO "${table}" ("id", "document") VALUES ('${document.id}','${JSON.stringify(document)}')`;
+      async save(table, document, id) {
+        try {
+          let query;
+          if (id) {
+            query = `UPDATE "${table}" SET document = '${JSON.stringify(document)}' where id = '${id}'`;
+          } else {
+            query = `INSERT INTO "${table}" ("id", "document") VALUES ('${document.id}','${JSON.stringify(document)}')`;
+          }
+          logger.debug(query);
+          return await pg.query(query);
+        } catch (err) {
+          logger.error(`error saving document: ${JSON.stringify(document)}, table: ${table}, id: ${id}`);
+          logger.error(err);
         }
+      },
+
+      async insertAggregateMeta(table, aggregate) {
+        let query = `INSERT INTO "${table}" ("id", "meta") VALUES ('${aggregate.id}','${JSON.stringify(aggregate)}')`;
         logger.debug(query);
         return await pg.query(query);
-      } catch (err) {
-        logger.error(`error saving document: ${JSON.stringify(document)}, table: ${table}, id: ${id}`);
-        logger.error(err);
-      }
-    },
+      },
 
-    async insertAggregateMeta(table, aggregate) {
-      let query = `INSERT INTO "${table}" ("id", "meta") VALUES ('${aggregate.id}','${JSON.stringify(aggregate)}')`;
-      logger.debug(query);
-      return await pg.query(query);
-    },
+      async getAggregateViewMeta(table, id) {
+        let query = (`SELECT * from "${table}" where "id" = '${id}'`);
+        logger.debug(query);
+        return await pg.query(query)
+          .then(result => {
+            const row = result.rows[0];
+            return row && row.meta ? row.meta : {};
+          });
+      },
 
-    async getAggregateViewMeta(table, id) {
-      let query = (`SELECT * from "${table}" where "id" = '${id}'`);
-      logger.debug(query);
-      return await pg.query(query)
-        .then(result => {
-          const row = result.rows[0];
-          return row && row.meta ? row.meta : {};
-        });
-    },
-
-    async saveAggregateView(table, aggregate, document) {
-      try {
-        let query;
-        if (document.id) {
-          query = `UPDATE "${table}" SET document = '${JSON.stringify(document)}' where id = '${document.id}';
+      async saveAggregateView(table, aggregate, document) {
+        try {
+          let query;
+          if (document.id) {
+            query = `UPDATE "${table}" SET document = '${JSON.stringify(document)}' where id = '${document.id}';
         INSERT INTO "${table}" ("id", "document") SELECT '${document.id}','${JSON.stringify(document)}'
         WHERE NOT EXISTS (SELECT 1 FROM "${table}" WHERE id = '${document.id}');`;
-        }
-        let updateAggSql = `UPDATE "${table}" SET meta = '${JSON.stringify(aggregate)}' where id = '${aggregate.id}'`;
-        let sql = `${query || ''};${updateAggSql}`;
-        logger.debug(sql);
-        return await pg.query(query);
-      } catch (err) {
-        logger.error(`error in saveAggregateView
+          }
+          let updateAggSql = `UPDATE "${table}" SET meta = '${JSON.stringify(aggregate)}' where id = '${aggregate.id}'`;
+          let sql = `${query || ''};${updateAggSql}`;
+          logger.debug(sql);
+          return await pg.query(query);
+        } catch (err) {
+          logger.error(`error in saveAggregateView
  aggregate: ${JSON.stringify(aggregate)},
  document: ${JSON.stringify(document)},
  table: ${table}`);
-        logger.error(err);
-      }
-    },
+          logger.error(err);
+        }
+      },
 
-    async saveSingletonAggregateView(table, aggregate, document, id) {
-      try {
-        let query = `UPDATE "${table}" SET meta = '${JSON.stringify(aggregate)}',
+      async saveSingletonAggregateView(table, aggregate, document, id) {
+        try {
+          let query = `UPDATE "${table}" SET meta = '${JSON.stringify(aggregate)}',
  document = '${JSON.stringify(document)}' where id = '${id}'`;
 
-        logger.debug(query);
-        return await pg.query(query);
-      } catch (err) {
-        logger.error(`error in saveSingletonAggregateView document: ${JSON.stringify(document)},
+          logger.debug(query);
+          return await pg.query(query);
+        } catch (err) {
+          logger.error(`error in saveSingletonAggregateView document: ${JSON.stringify(document)},
  aggregate: ${JSON.stringify(aggregate)}
  table: ${table},
   id: ${id}`);
-        logger.error(err);
-      }
-    },
+          logger.error(err);
+        }
+      },
 
-    async saveQuery(query) {
-      try {
+      async saveQuery(query) {
+        try {
+          logger.debug(query);
+
+          return await pg.query(query);
+
+        } catch (err) {
+          logger.error(`error in savingQuery query: ${query}`);
+          logger.error(err);
+        }
+      },
+
+      async query(query) {
         logger.debug(query);
-
-        return await pg.query(query);
-
-      } catch (err) {
-        logger.error(`error in savingQuery query: ${query}`);
-        logger.error(err);
+        return await pg.query(query)
+          .then(result => {
+            const row = result.rows[0];
+            return row && row.meta ? row.meta : {};
+          });
       }
-    },
-
-    async query(query) {
-      logger.debug(query);
-      return await pg.query(query)
-        .then(result => {
-          const row = result.rows[0];
-          return row && row.meta ? row.meta : {};
-        });
-    }
+    };
   };
 };
