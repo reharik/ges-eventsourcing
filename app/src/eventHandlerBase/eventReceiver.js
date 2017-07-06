7012f8c756e2cab30998501319fa1ab17fc45d31
Raif Harik <reharik@gmail.com>
2017-07-06 11:29:31 -0500

    tired of bullshit

--- app/src/eventHandlerBase/eventReceiver.js
+++ app/src/eventHandlerBase/eventReceiver.js
@@ -5,6 +5,7 @@ module.exports = function(eventWorkflow, concurrentqueue) {
     const processor = x => {
       const func = handler[x.eventName];
       if (func) {
+        console.log('process event');
         return eventWorkflow(x, handler.handlerName, func.bind(handler));
       }
       return Promise.resolve();
