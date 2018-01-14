fa3a7b02002bf368edeee8342be8c46d2ef99eb9
Raif Harik <reharik@gmail.com>
2018-01-14 10:39:59 -0600

    lets try this

--- app/src/eventStore/pingES.js
+++ app/src/eventStore/pingES.js
@@ -1,5 +1,4 @@
 module.exports = function(
-  nodeeventstoreclient,
   promiseretry,
   superagent) {
 
@@ -18,8 +17,44 @@ module.exports = function(
 
   return options => {
     return promiseretry(function(retry, number) {
+      console.log(retry);
       console.log('es connect attempt number', number);
       return ping(options).catch(retry);
     }, {retries: 10});
   };
 };
+
+
+/************** run in console *********
+
+const promiseretry = require('promise-retry');
+const superagent = require('superagent');
+
+const opts = {
+  "host": "localhost",
+    "http": "eventstore:2113",
+    "systemUsers": {
+    "admin": "admin",
+      "adminPassword": "changeit"
+  }
+};
+
+  const ping = options => {
+    return superagent
+      .get(`${options.http}/streams/$projections-$all/0`)
+      .set('Accept', 'application/vnd.eventstore.atom+json')
+      .auth(options.systemUsers.admin, options.systemUsers.adminPassword)
+      .then(function(res) {
+        console.log(`=========="EventStore connection available"=========`);
+        console.log("EventStore connection available"); // eslint-disable-line quotes
+        console.log(`==========END "EventStore connection available"=========`);
+        return res.body;
+      });
+  };
+
+    promiseretry(function(retry, number) {
+      console.log(retry);
+      console.log('es connect attempt number', number);
+      return ping(opts).catch(retry);
+    }, {retries: 10});
+ */
