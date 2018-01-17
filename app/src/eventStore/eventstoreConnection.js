0f40560155328cb600199777ff9bac1fc9ed4fa2
Raif Harik <reharik@gmail.com>
2018-01-17 10:50:46 -0600

    tweakin the connection

--- app/src/eventStore/eventstoreConnection.js
+++ app/src/eventStore/eventstoreConnection.js
@@ -7,19 +7,21 @@ module.exports = function(nodeeventstoreclient, pingES, logger) {
         {verbose: options.verbose, log: logger},
         {host: options.host, port: 1113});
 
-      gesConnection.on('closed', reason => {
-        logger.info(`ES gesConnection: ${gesConnection._connectionName} closed, reason:`, reason);
-        throw new Error(reason);
-      });
-
-      gesConnection.connect();
-      return new Promise(res => {
-        return gesConnection.once('connected', tcpEndPoint => {
+      await gesConnection.connect();
+      return new Promise((res, rej) => {
+        gesConnection.once('closed', rej);
+        gesConnection.once('connected', tcpEndPoint => {
+          gesConnection.removeListener('closed', rej);
           console.log(`=========='connected'=========`);
           console.log('connected to eventstore');
           console.log(`==========END 'connected'=========`);
           logger.trace(`gesConnection: ${gesConnection._connectionName}
  - ${JSON.stringify(tcpEndPoint, null, 4)}`);
+          gesConnection.on('closed', reason => {
+            logger.info(`ES gesConnection: ${gesConnection._connectionName} closed, reason:`, reason);
+            logger.info('Connection to GES lost. Terminating this process.');
+            process.exit(-1); //eslint-disable-line no-process-exit
+          });
           return res(gesConnection);
         });
       })
