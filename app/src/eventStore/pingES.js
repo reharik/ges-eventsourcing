e87aefa75773d357e97cc191baee85d639a73fe2
Raif Harik <reharik@gmail.com>
2018-11-24 10:01:21 -0600

    touch up

--- app/src/eventStore/pingES.js
+++ app/src/eventStore/pingES.js
@@ -1,36 +1,7 @@
-module.exports = function(
-  promiseretry,
-  superagent) {
-
-  const ping = options => {
-    return superagent
-      .get(`${options.http}/streams/$projections-$all/0`)
-      .set('Accept', 'application/vnd.eventstore.atom+json')
-      .auth(options.systemUsers.admin, options.systemUsers.adminPassword)
-      .then(function(res) {
-        console.log(`=========="EventStore connection available"=========`);
-        console.log("EventStore connection available"); // eslint-disable-line quotes
-        console.log(`==========END "EventStore connection available"=========`);
-        return res.body;
-      });
-  };
-
-  return options => {
-    return promiseretry(function(retry, number) {
-      console.log('es connect attempt number', number);
-      return ping(options).catch(err => {
-        console.log(err);
-        retry(err);
-      });
-    }, {retries: options.retryCount || 5});
-  };
-};
-
-
 module.exports = function(superagent, config, asyncretry) {
   const configs = config.configs.children.eventstore;
   const ping = async function(bail, number) {
-    console.log('attempt to connect to the ES number', number, Date.now().toString());
+    console.log('attempt to connect to the ES number', number, new Date().toString());
     await superagent
       .get(`${configs.http}/streams/$projections-$all/0`)
       .set('Accept', 'application/vnd.eventstore.atom+json')
